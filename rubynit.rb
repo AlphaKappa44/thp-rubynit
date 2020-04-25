b247aaaf701b4eaa1cbd697cfa778ab979b8c870
Bento <oobento01oo@xn--mail-uoa.com>
2020-04-25 11:00:20 +0200

    Add new features

--- rubynit.rb
+++ rubynit.rb
@@ -1,3 +1,23 @@
+README = "# The Hacking Project (Week [i?] - Day [i?])
+
+## Project : [project_name?]
+
+/!\\ Be careful ! Configure the ruby version in Gemfile to adapt the program to your own ruby version /!\\
+
+### Installation
+
+To install the project, follow these steps :
+1. Use command `git clone` to clone the project on your environnment\n
+2. Use command `CD` to go in the project's repository\n
+3. Use command `bundle install` to install all the gem dependancies\n
+
+Then, you can :
+- Launch the program thanks to the command `ruby app.rb`\n
+- Use the code linter to get a clean code by using command `rubocop`
+
+### Presentation"
+
+
 # Create a folder named by user's ARGV
 def make_project_folder
   # Check user input
@@ -12,7 +32,7 @@ end
 
 # If user don't input ARGV, explain how the command works then exit
 def check_if_user_gave_input
-  abort('mkdir : missing input. Retry using rubynit project_name') if ARGV.empty?
+  abort('[rubynit] Missing parameter project_name. Retry using command `rubynit project_name`') if ARGV.empty?
 end
 
 # Make Gemfile with THP's usual gems
@@ -23,6 +43,23 @@ def make_gemfile(project_name)
   file.puts "gem 'rspec'"
   file.puts "gem 'pry'"
   file.puts "gem 'rubocop', '~> 0.57.2'"
+  file.close
+  path = system("pwd")
+end
+
+# Make Gemfile with THP's usual gems
+def make_readme(project_name)
+  file = File.open("#{project_name}/README.md", 'w')
+  file.puts README
+  file.close
+  path = system("pwd")
+end
+
+# Make prefilled app.rb file
+def make_app_file(project_name)
+  file = File.open("#{project_name}/app.rb", 'w')
+  file.puts "require 'bundler'"
+  file.puts "Bundler.require"
   file.close
   path = system("pwd")
 end
@@ -50,6 +87,11 @@ def make_lib_folder(project_name)
   Dir.mkdir("#{project_name}/lib")
 end
 
+# Make lib folder
+def make_db_folder(project_name)
+  Dir.mkdir("#{project_name}/db")
+end
+
 # Run rspec --init
 def rspec_init(project_name)
   system("cd #{project_name} && rspec --init")
@@ -69,6 +111,12 @@ def perform
   # Make Gemfile with THP's usual gems
   make_gemfile(project_name)
 
+  # Make prefilled README.md
+  make_readme(project_name)
+
+  # Make prefilled app.rb
+  make_app_file(project_name)
+
   # Make .env file
   make_env_file(project_name)
 
@@ -77,6 +125,9 @@ def perform
 
   # Make lib folder
   make_lib_folder(project_name)
+
+  # Make db folder
+  make_db_folder(project_name)
 
   # Run rspec --init
   rspec_init(project_name)
